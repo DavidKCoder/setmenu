b4598445aa58061799dacc9423a1c14379556553
DavidKCoder <karapetyand91@gmail.com>
2025-07-03 17:01:56 +0400

    Add Vercel Analytics.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import BackToTopButton from "@/components/BackToTopButton";
 import Providers from "@/app/providers";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
     title: "SetMenu",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         <body className="max-w-screen-2xl m-auto overflow-x-hidden scroll-smooth">
         <Providers>
             {children}
+            <Analytics />
             <Footer />
             <BackToTopButton />
         </Providers>
