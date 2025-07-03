eb6d74b7a3b1a8390d34142418033add65b993be
DavidKCoder <karapetyand91@gmail.com>
2025-07-03 11:40:26 +0400

    Change statistics page.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -8,24 +8,24 @@ export default function Stats() {
         <div className="bg-white py-10 sm:py-10">
             <div
                 className="sm:bg-fixed bg-cover bg-center py-16 sm:py-16"
-                style={{ backgroundImage: "url('https://images.pexels.com/photos/17294720/pexels-photo-17294720.jpeg')" }}
+                style={{ backgroundImage: "url('https://images.pexels.com/photos/14280792/pexels-photo-14280792.jpeg')" }}
             >
                 <div className="bg-white/80 backdrop-blur-md rounded-xl mx-auto max-w-7xl px-6 py-16 lg:px-8">
                     <dl className="grid grid-cols-3 sm:grid-cols-3 gap-x-8 gap-y-16 text-center">
                         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                            <dt className={`text-xl leading-7 text-white bg-black p-1 uppercase ${forum_splash.className}`}>Restaurants Listed</dt>
+                            <dt className={`text-xl leading-7 text-white p-1 uppercase ${forum_splash.className}`}>Restaurants Listed</dt>
                             <dd className={`order-first text-4xl font-bold tracking-tight text-white sm:text-6xl italic ${splash.className}`}>
                                 <CountUp end={50} duration={2.5} separator="," suffix="+" enableScrollSpy />
                             </dd>
                         </div>
                         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                            <dt className={`text-xl leading-7 text-white bg-black p-1 uppercase ${forum_splash.className}`}>Menus Available</dt>
+                            <dt className={`text-xl leading-7 text-white p-1 uppercase ${forum_splash.className}`}>Menus Available</dt>
                             <dd className={`order-first text-4xl font-bold tracking-tight text-white sm:text-6xl italic ${splash.className}`}>
                                 <CountUp end={100} duration={2.5} separator="," suffix="+" enableScrollSpy />
                             </dd>
                         </div>
                         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                            <dt className={`text-xl leading-7 text-white bg-black p-1 uppercase ${forum_splash.className}`}>Monthly Users</dt>
+                            <dt className={`text-xl leading-7 text-white p-1 uppercase ${forum_splash.className}`}>Monthly Users</dt>
                             <dd className={`order-first text-4xl font-bold tracking-tight text-white sm:text-6xl italic ${splash.className}`}>
                                 <CountUp end={2500} duration={2.5} separator="," suffix="+" enableScrollSpy />
                             </dd>
