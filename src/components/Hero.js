5c1cbab4d7db524a17189bd1431b563603046a57
DavidKCoder <karapetyand91@gmail.com>
2025-07-08 01:02:50 +0400

    Change main bg image.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,10 @@ import { useTranslation } from "next-i18next";
 
 export default function Hero() {
     const { t } = useTranslation();
-    const urlImage = `url('https://cdn0.weddingwire.com/article/3408/original/1280/jpg/18043-chart-house.jpeg')`;
+    // const url = `url('https://images.pexels.com/photos/16120242/pexels-photo-16120242.jpeg')`;
+    // const url = `url('https://cdn0.weddingwire.com/article/3408/original/1280/jpg/18043-chart-house.jpeg')`;
+
+    const urlImage = `url('https://images.pexels.com/photos/32179371/pexels-photo-32179371.jpeg')`;
 
     return (
         <div
@@ -17,7 +20,8 @@ export default function Hero() {
             <div className="absolute inset-0 bg-black/50"></div>
 
             {/* Text Content */}
-            <div className="relative z-10 flex flex-col items-center justify-center text-center px-4 py-12 sm:px-8 sm:py-20">
+            <div
+                className="relative z-10 flex flex-col items-center justify-center text-center px-4 py-12 sm:px-8 sm:py-20">
                 <h1 className="text-3xl sm:text-5xl md:text-6xl font-extrabold text-white max-w-2xl uppercase">
                     {t("title")}
                 </h1>
@@ -30,7 +34,8 @@ export default function Hero() {
             <div className="relative z-10 flex justify-center px-4 pb-10 sm:pb-20">
                 <RestaurantSearchBar
                     restaurants={restaurants}
-                    onResults={() => {}}
+                    onResults={() => {
+                    }}
                 />
             </div>
         </div>
