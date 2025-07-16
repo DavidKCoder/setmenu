2773d279116ab7001174f0e5f3fa5216b7f23a28
DavidKCoder <karapetyand91@gmail.com>
2025-07-16 11:53:38 +0400

    Add icons instead of numbers.

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -3,28 +3,36 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { forum_splash, splash } from "@/app/fonts";
+import { FiSearch } from "react-icons/fi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { FaMapMarkedAlt, FaUtensils, FaSmileBeam } from "react-icons/fa";
 
-export default function HowItWorks() {
+const HowItWorks = React.memo(() => {
     const { t } = useTranslation();
 
     const steps = [
         {
+            icon: <FiSearch  />,
             title: t("howSteps.discoverTitle"),
             description: t("howSteps.discoverDescription"),
         },
         {
+            icon: <MdOutlineRestaurantMenu  />,
             title: t("howSteps.selectTitle"),
             description: t("howSteps.selectDescription"),
         },
         {
+            icon: <FaMapMarkedAlt  />,
             title: t("howSteps.exploreTitle"),
             description: t("howSteps.exploreDescription"),
         },
         {
+            icon: <FaUtensils  />,
             title: t("howSteps.setTitle"),
             description: t("howSteps.setDescription"),
         },
         {
+            icon: <FaSmileBeam  />,
             title: t("howSteps.enjoyTitle"),
             description: t("howSteps.enjoyDescription"),
         },
@@ -39,14 +47,15 @@ export default function HowItWorks() {
                     {t("howItWorks")}
                 </h1>
                 <div className="grid grid-cols-2 md:grid-cols-5 gap-5 sm:gap-10">
-                    {steps.map(({ title, description }, idx) => (
+                    {steps.map(({ icon, title, description }, idx) => (
                         <div
                             key={idx}
                             className="flex flex-col items-center text-center px-2"
                         >
                             <div
-                                className={`w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-main text-white flex items-center justify-center text-xl sm:text-2xl font-bold mb-4 ${splash.className}`}>
-                                {idx + 1}
+                                className={`w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-main text-white flex items-center justify-center text-xl sm:text-2xl font-bold mb-4 ${splash.className}`}
+                            >
+                                {icon}
                             </div>
                             <h2
                                 className={`text-lg sm:text-2xl mb-2 text-gray-700 ${forum_splash.className}`}
@@ -54,11 +63,15 @@ export default function HowItWorks() {
                             >
                                 {title}
                             </h2>
-                            <p className="text-gray-700 text-xs sm:text-sm">{description}</p>
+                            <p className="text-gray-700 text-xs sm:text-sm">
+                                {description}
+                            </p>
                         </div>
                     ))}
                 </div>
             </div>
         </main>
     );
-}
+});
+
+export default HowItWorks;
